77c00686daf16b57cb989ee4d4192cd0d7fd6dcb
holeminh <minhhl@japanquality.asia>
2020-09-15 11:37:12 +0700

    refs

---
+++ songs.js
@@ -0,0 +1,19 @@
+var songs = [{
+		artist: "Thùy Chi",
+		name: "Lời Chưa Nói",
+		url: "https://drive.google.com/uc?export=download&id=1_oUwVxLRxyyf9GlD3yTtGC5Vu9bgyEfP",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},
+	{
+		artist: "Soobin Hoàng Sơn",
+		name: "Say Goodbye",
+		url: "https://drive.google.com/uc?export=download&id=1F-smbuj__v7_1Q-Mag05-dQJWD77q6w4",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},
+	{
+		artist: "Trịnh Đình Quang",
+		name: "Nếu Không Thể Đến Với Nhau",
+		url: "https://drive.google.com/uc?export=download&id=1QiKiH_ibtxgUmULtKzeBLdJ-K03BpOUR",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	}
+];
